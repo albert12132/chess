a82f64072343eab7f515749525f0087449914a4f
Albert Wu <albert12132@gmail.com>
2025-12-24 10:06:49 -0800

    remove base path in order to use custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/chess/',
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
